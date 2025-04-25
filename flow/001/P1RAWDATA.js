cf38b72c9e46c3f4e1096f5339b9e0585b60fbcd
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2025-04-25 12:48:15 +0700

    .

--- flow/001/P1RAWDATA.js
+++ flow/001/P1RAWDATA.js
@@ -275,10 +275,10 @@ router.post('/RAWDATA/insertdata', async (req, res) => {
   //     ,[Picture]
   //     ,[UserInput]
   //mssqlR
-  if(input["Location"] != undefined && input["Plant"] != undefined&& input["Order"] != undefined&& input["CP"] != undefined&& input["FG"] != undefined&& input["ItemsCode"] != undefined&& input["ItemsName"] != undefined&& input["NUMBER"] != undefined&& input["POINT"] != undefined&& input["Data"] != undefined&& input["Picture"] != undefined&& input["SEQ"] != undefined   && input["QTYT"] != undefined && input["UNIT"] != undefined  && input["CUSTNA"] != undefined && input["PARTNA"] != undefined  && input["PARTNO"] != undefined && input["PROC"] != undefined  && input["CUSLOTNO"] != undefined && input["FG_CHARG"] != undefined && input["CUST_FULLNM"] != undefined&& input["TYPE"] != undefined ){
+  if(input["Location"] != undefined && input["Plant"] != undefined&& input["Order"] != undefined&& input["CP"] != undefined&& input["FG"] != undefined&& input["ItemsCode"] != undefined&& input["ItemsName"] != undefined&& input["NUMBER"] != undefined&& input["POINT"] != undefined&& input["Data"] != undefined&& input["Picture"] != undefined&& input["SEQ"] != undefined   && input["QTYT"] != undefined && input["UNIT"] != undefined  && input["CUSTNA"] != undefined && input["PARTNA"] != undefined  && input["PARTNO"] != undefined && input["PROC"] != undefined  && input["CUSLOTNO"] != undefined && input["FG_CHARG"] != undefined && input["CUST_FULLNM"] != undefined&& input["TYPE"] != undefined&& input["INSTRUMENT"] != undefined ){
     let queryS = `INSERT INTO [RAWDATA].[dbo].[autorawdata] 
-    ([Location],[Plant],[Order],[CP],[FG],[ItemsCode],[ItemsName],[NUMBER],[POINT],[Data],[Picture],[UserInput] ,[SEQ],[QTYT],[UNIT],[CUSTNA],[PARTNA],[PARTNO],[PROC],[CUSLOTNO],[FG_CHARG],[CUST_FULLNM],[TYPE]) 
-    VALUES ('${input["Location"]}','${input["Plant"]}','${input["Order"]}','${input["CP"]}','${input["FG"]}','${input["ItemsCode"]}','${input["ItemsName"]}','${input["NUMBER"]}','${input["POINT"]}','${input["Data"]}','${input["Picture"]}','${input["UserInput"]}','${input["SEQ"]}','${input["QTYT"]}','${input["UNIT"]}','${input["CUSTNA"]}','${input["PARTNA"]}','${input["PARTNO"]}','${input["PROC"]}','${input["CUSLOTNO"]}','${input["FG_CHARG"]}','${input["CUST_FULLNM"]}','${input["TYPE"]}');`;
+    ([Location],[Plant],[Order],[CP],[FG],[ItemsCode],[ItemsName],[NUMBER],[POINT],[Data],[Picture],[UserInput] ,[SEQ],[QTYT],[UNIT],[CUSTNA],[PARTNA],[PARTNO],[PROC],[CUSLOTNO],[FG_CHARG],[CUST_FULLNM],[TYPE],[SP01]) 
+    VALUES ('${input["Location"]}','${input["Plant"]}','${input["Order"]}','${input["CP"]}','${input["FG"]}','${input["ItemsCode"]}','${input["ItemsName"]}','${input["NUMBER"]}','${input["POINT"]}','${input["Data"]}','${input["Picture"]}','${input["UserInput"]}','${input["SEQ"]}','${input["QTYT"]}','${input["UNIT"]}','${input["CUSTNA"]}','${input["PARTNA"]}','${input["PARTNO"]}','${input["PROC"]}','${input["CUSLOTNO"]}','${input["FG_CHARG"]}','${input["CUST_FULLNM"]}','${input["TYPE"]}','${input["INSTRUMENT"]}');`;
 
     console.log(queryS)
     let db = await mssqlR.qureyR(queryS);
@@ -410,6 +410,54 @@ router.post('/RAWDATA/getrawreport', async (req, res) => {
   return res.json(output);
 });
 
+router.post('/RAWDATA/DELETELASTrawreport', async (req, res) => {
+  //-------------------------------------
+  console.log("--RAWDATA/DELETELASTrawreport--");
+  console.log(req.body);
+  let input = req.body;
+  //-------------------------------------
+  let date = Date.now()
+  let output = [];
 
+  // ,[Location]
+  //     ,[Plant]
+  //     ,[Order]
+  //     ,[CP]
+  //     ,[FG]
+  //     ,[ItemsCode]
+  //     ,[ItemsName]
+  //     ,[DataNo]
+  //     ,[Data]
+  //     ,[Picture]
+  //     ,[UserInput]
+  //mssqlR
+  if(input["Order"] != undefined){
+    // let queryS = `delete from [RAWDATA].[dbo].[autorawdata] where [Order]= '${input["Order"]}' order by date desc limit 1`;
+    let queryS = `DELETE FROM [RAWDATA].[dbo].[autorawdata]
+                  WHERE [date] IN (
+                      SELECT TOP 1 [date] 
+                      FROM [RAWDATA].[dbo].[autorawdata]
+                      WHERE [Order] = '${input["Order"]}'
+                      ORDER BY [date] DESC
+                  );`;
+
+    console.log(queryS)
+    let db = await mssqlR.qureyR(queryS);
+    console.log(db)
+    // if (db['recordsets'].length > 0) {
+    //   let datadb = db['recordsets'][0];
+    //   output = datadb
+    // }
+  }
+  
+
+
+  //-------------------------------------
+  return res.json(output);
+});
+
+// delete from marks order by id desc limit 1
 
 module.exports = router;
+
+
